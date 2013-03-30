14b0df61743cd0249574247c8fcd640858661ab8
Sudipta Mondal <cloud@cloud-Lenovo-G570.(none)>
2013-03-30 23:29:48 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NotesApp::Application.config.secret_token = 'df44ea7308e3311d55482a3e5b35f6bfa3b42f6994e4d220da955f1fd1f245b6ee91b5163e705908b25fe2ff463c8a1c39ccce16c61a0e1dac26ea19d10f6cd4'
